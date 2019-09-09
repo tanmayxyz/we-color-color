fc1dfaf7e755fd34e355121d59f4ed3109799b4e
tanmayxyz <tanmaymaityxyz@gmail.com>
2019-09-09 15:28:36 +0530

    created pallete route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import SeedColor from "./SeedColor";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Pallete from "./Pallete";
 import { generatePalette } from "./Colorhelpers";
-function App() {
-  return (
-    <div>
-      <Pallete pallete={generatePalette(SeedColor[4])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return SeedColor.find(palette => {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>pallete goes here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Pallete
+              pallete={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Pallete pallete={generatePalette(SeedColor[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
